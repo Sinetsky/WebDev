ab6c9df1f01e7d7044f0e6bce1ca25f6cbd3a94b
Sinetsky <91119320+Sinetsky@users.noreply.github.com>
2021-10-20 23:53:18 +0300

    Add files via upload

---
+++ Think-bin/script.js
@@ -0,0 +1,6 @@
+function post() {
+  var name = document.getElementById("name").value;
+  var think = document.getElementById("think").value;
+  var output = document.getElementById("output").innerHTML = "Hello " + name + "," + " Nice post!";
+  
+}
